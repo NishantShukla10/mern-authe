fe87aa955ccba1eeacacc6f2e7f5bc691938d9d7
Nishant Shukla <nishantshukla332@gmail.com>
2025-10-17 22:28:15 +0530

    Updated CORS

--- server/server.js
+++ server/server.js
@@ -3,25 +3,38 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
-import authRouter from './routes/authRoutes.js'
+import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-
 
 const app = express();
 const port = process.env.PORT || 4000;
-connectDB();
 
-const allowedOrigins = [
-    'http://localhost:5173'
-]
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
 
-// API endpoints
-app.get('/', (req, res)=> res.send('API Working2!'))
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter); 
+const allowedOrigins = process.env.CLIENT_URLS
+  ? process.env.CLIENT_URLS.split(",")
+  : [];
 
-app.listen(port, ()=> console.log(`Server started on PORT: ${port}`))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.warn("Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => res.send("API Working2!"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
